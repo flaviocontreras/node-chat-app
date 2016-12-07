17fe49efdd6e88eb6e80d6daa323be81c1f1134a
Flavio Contreras <flaviohcontreras@live.com>
2016-12-06 23:32:11 -0200

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(PUBLIC_PATH));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'See you then',
-    createdAt: 321
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', (socket) => {
